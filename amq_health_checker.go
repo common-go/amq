b2863784ff7d9d0a58990c43e21e531097dc1a4b
Tong Con Duong <conduongtong@yahoo.com>
2020-10-19 15:23:13 +0700

    change health service to health checker and update

--- amq_health_checker.go
+++ amq_health_checker.go
@@ -2,34 +2,35 @@ package amq
 
 import (
 	"context"
+
 	"github.com/go-stomp/stomp"
 )
 
-type AMQHealthService struct {
+type AMQHealthCheck struct {
 	Conn *stomp.Conn
 	name string
 	des  string
 }
 
-func NewAMQHealthService(conn *stomp.Conn, name string, des string) *AMQHealthService {
+func NewAMQHealthCheck(conn *stomp.Conn, name string, des string) *AMQHealthCheck {
 	if len(name) == 0 {
 		name = "amq"
 	}
 	if len(des) == 0 {
 		des = "Test::Message"
 	}
-	return &AMQHealthService{conn, name, des}
+	return &AMQHealthCheck{conn, name, des}
 }
 
-func NewDefaultAMQHealthService(client *stomp.Conn, des string) *AMQHealthService {
-	return &AMQHealthService{client, "amq", des}
+func NewDefaultAMQHealthCheck(client *stomp.Conn, des string) *AMQHealthCheck {
+	return &AMQHealthCheck{client, "amq", des}
 }
 
-func (s *AMQHealthService) Name() string {
+func (s *AMQHealthCheck) Name() string {
 	return s.name
 }
 
-func (s *AMQHealthService) Check(ctx context.Context) (map[string]interface{}, error) {
+func (s *AMQHealthCheck) Check(ctx context.Context) (map[string]interface{}, error) {
 	res := make(map[string]interface{})
 	subscription, err := s.Conn.Subscribe(s.des, stomp.AckAuto,
 		stomp.SubscribeOpt.Header("subscription-type", "ANYCAST"),
@@ -42,7 +43,7 @@ func (s *AMQHealthService) Check(ctx context.Context) (map[string]interface{}, e
 	return res, err
 }
 
-func (s *AMQHealthService) Build(ctx context.Context, data map[string]interface{}, err error) map[string]interface{} {
+func (s *AMQHealthCheck) Build(ctx context.Context, data map[string]interface{}, err error) map[string]interface{} {
 	if err == nil {
 		return data
 	}
